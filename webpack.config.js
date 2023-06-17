58c0583fbd4ad601a384c490711cd8a4091fd579
Pradeep Saravanan <79619944+developerpradeepofficial@users.noreply.github.com>
2023-06-17 16:19:05 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const cesiumWorkers = "../Build/Cesium/Workers";
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
-// var favicon = require("favicons-webpack-plugin");
+const favicon = require("favicons-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -37,7 +37,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
-    // new favicon("./Assets/Images/favicon/favicon.ico"),
+    new favicon("Assets/Images/favicon/favicon.ico"),
     // Copy Cesium Assets, Widgets, and Workers to a static directory
     new CopyWebpackPlugin({
       patterns: [
