0a731501bd9536dd12e13a13b1eda0fd459bc7d8
Pradeep Saravanan <pradeepsaridha@gmail.com>
2023-07-10 10:05:23 +0530

    Refactor

--- src/index.js
+++ src/index.js
@@ -840,39 +840,3 @@ function visualizeAndFocus(e) {
     });
   };
 
-  // const polyline = viewer.entities.add({
-  //   polyline: {
-  //     positions: Cesium.Cartesian3.fromDegreesArray([
-  //       -75.60217330403601, 40.04102882709425, -75.59968252414251,
-  //       40.04093615560871, -75.598020153828, 40.04079437042357,
-  //       -75.59674934074435, 40.040816173283304, -75.59630042791713,
-  //       40.03986900370842, -75.59563636849978, 40.03930996506271,
-  //       -75.59492397899098, 40.03873932846581, -75.59457991226778,
-  //       40.038392701955786, -75.59424838652453, 40.03775403572295,
-  //       -75.59387104290336, 40.03677022167725, -75.59355000490342,
-  //       40.03588760913535,
-  //     ]),
-  //     width: 8,
-  //     material: new Cesium.PolylineOutlineMaterialProperty({
-  //       color: Cesium.Color.YELLOW,
-  //       outlineWidth: 2,
-  //       outlineColor: Cesium.Color.BLACK,
-  //     }),
-  //     clampToGround: true,
-  //   },
-  // });
-}
-
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-// viewer.scene.primitives.add(createOsmBuildings());
-
-// Create a new entity
-//sets default view
-
-//REMOVE BING IMAGERY
-// const viewModel = viewer.baseLayerPicker.viewModel;
-// viewModel.imageryProviderViewModels =
-//   viewModel.imageryProviderViewModels.filter((el) => {
-//     return el.category !== "Cesium ion";
-//   });
-// viewModel.selectedImagery = viewModel.imageryProviderViewModels[2]; //select default imageryProvider
